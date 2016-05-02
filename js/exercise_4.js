524764abe489f9c94f1649369b2247bfa74c3e14
clavicon <calvinharmin@gmail.com>
2016-05-02 17:42:36 -0400

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -127,7 +127,9 @@ function getDirections(frm, to) {
           {lat: to[1], lon: to[0]}
         ],
       	costing: 'auto',
-      	units: 'miles'
+      	directions_options:{
+          	units: 'miles'
+        }
     })
     $.ajax({
     	url: 'https://valhalla.mapzen.com/route',
