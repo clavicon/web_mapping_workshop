1266c46aca23bb871c8d3d7bd652628b6a507e05
clavicon <calvinharmin@gmail.com>
2016-05-02 16:08:21 -0400

    added popup fancy stuff

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -19,10 +19,12 @@ var map = L.mapbox.map('map', mapId);
 
 var dataFileToAdd = 'data/restaurants.geojson';
 
-var featureLayer = L.mapbox.featureLayer();
-	featureLayer.loadURL(dataFileToAdd);
-	featureLayer.addTo(map);
-	
+var featureLayer = L.mapbox.featureLayer();  //empty feature layer object
+	featureLayer.loadURL(dataFileToAdd);	//load the data into this object
+	featureLayer.addTo(map);	//now add this layer with data into the map element
+
+//when the feature is 'ready' and loaded, each feature (which is called a Layer here for some reason in eachLayer)
+//is given an icon with color, size, and symbol
 featureLayer.on('ready', function(){
 	this.eachLayer(function(layer){
     	layer.setIcon(L.mapbox.marker.icon({
@@ -31,9 +33,11 @@ featureLayer.on('ready', function(){
           	"marker-symbol": "restaurant"
         }))
     })
-    map.fitBounds(featureLayer.getBounds());
+  	//the map extent is set to the extent of the data loaded in featureLayer
+    map.fitBounds(featureLayer.getBounds());	
 })
 
+//this section is commented out because we are doing something more complicated on 'click' below
 /*
 featureLayer.on('ready', function(){
 	this.eachLayer(function(layer){
@@ -42,6 +46,14 @@ featureLayer.on('ready', function(){
 })
 */
 
+// this is a function for what to do when a feature is clicked on
+// instead of a popup it creates a sidebar with information
+// so this function must grab the geojson information like "phone number" and such
+// and create a <div> and fill it with that information; so a lot of this function is 
+// actually translating the geojson info into HTML to append into the new <div> being created
+// and this <div> sits inside the sidebar <div> that has already been defined in index.html.
+// The sidebar then fades out when you click off somewhere in the empty 'map' object, which
+// in this case is anywhere in the map that is not a feature. 
 var clickHandler = function(e){
 	$('#info').empty();
    
