a32211f075122d81abdcc28c8e8a16a2781074ff
clavicon <calvinharmin@gmail.com>
2016-05-02 17:38:50 -0400

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -149,7 +149,7 @@ function getDirections(frm, to) {
            		"stroke-width": 8
          	}
         })
-        $('#directions').fadeIn(400, funtion(){
+        $('#directions').fadeIn(400, function(){
 			var summary = data.trip.summary
 			$('#summary').empty();
 			$('#distance').text(Math.round(summary.length * 100)/100 + ' ' + data.trip.units);
